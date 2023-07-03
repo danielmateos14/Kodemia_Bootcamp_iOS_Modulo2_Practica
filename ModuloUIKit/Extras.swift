c18eab23b5adddbfe20023b2655b050cb4d7d806
Daniel Mateos <luisdmateos@outlook.com>
2023-07-02 19:18:42 -0600

    Initial Commit

---
+++ ModuloUIKit/Extras.swift
@@ -0,0 +1,22 @@
+//
+//  Extras.swift
+//  ModuloUIKit
+//
+//  Created by Daniel Mateos on 28/06/23.
+//
+
+import Foundation
+import UIKit
+
+class Extras{
+    static let extras = Extras()
+    
+    var myViewBackgroundColor = UIColor(red: 31/255, green: 25/255, blue: 22/255, alpha: 1)
+    var myViewLoginBackgroundColor = UIColor(red: 235/255, green: 231/255, blue: 212/255, alpha: 1)
+    var butonTitleBackgroundColor = UIColor(red: 227/255, green: 208/255, blue: 128/255, alpha: 1)
+    var googleRedColor = UIColor(red: 195/255, green: 95/255, blue: 62/255, alpha: 1)
+    var facebookBlueColor = UIColor(red: 74/255, green: 108/255, blue: 231/255, alpha: 1)
+    var loginButtonColor = UIColor(red: 227/255, green: 208/255, blue: 128/255, alpha: 1) //227 208 128 
+    var signButtonColor = UIColor(red: 53/255, green: 48/255, blue: 45/255, alpha: 1) //53 48 45
+    var titleColor = UIColor(red: 207/255, green: 105/255, blue: 35/255, alpha: 1) //53 48 45 
+}
